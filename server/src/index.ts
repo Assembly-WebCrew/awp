c58686c01e742bd91231b0f8c5b4f0becfc50e2c
Joona Romppanen <joona.romppanen@gmail.com>
2016-09-24 18:19:50 +0300

    Add server scaffold

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello, world');
+import * as express from 'express';
+import * as socketIo from 'socket.io';
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data: any) {
+    console.log(data);
+  });
+});
+
+server.listen(8080, () => {
+  console.log(`Servering listening at ${server.address().address}:${server.address().port}`)
+});
